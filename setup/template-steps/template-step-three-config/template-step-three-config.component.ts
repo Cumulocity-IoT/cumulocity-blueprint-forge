bd7668572e865d482ecfffc0aea7b56c239cd67c
Darpankumar Lalani <darpankumar.lalani@softwareag.com>
2023-06-13 18:09:18 +0530

    updated working copy

--- setup/template-steps/template-step-three-config/template-step-three-config.component.ts
+++ setup/template-steps/template-step-three-config/template-step-three-config.component.ts
@@ -17,6 +17,7 @@ import { WidgetCatalogService } from '../../../builder/widget-catalog/widget-cat
 import { DashboardWidgets, Dashboards, MicroserviceDetails, PluginDetails, TemplateBlueprintDetails, TemplateBlueprintEntry } from './../../template-setup.model';
 import { ApplicationBinaryService } from '../../../builder/application-binary.service';
 import { TemplateCatalogService } from '../../../builder/template-catalog/template-catalog.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'c8y-template-step-three-config',
@@ -40,7 +41,7 @@ export class TemplateStepThreeConfigComponent extends TemplateSetupStep {
 
   app: Observable<any>;
   refreshApp = new BehaviorSubject<void>(undefined);
-
+  currentApp: IApplication;
   constructor(
     public stepper: C8yStepper,
     protected step: CdkStep,
@@ -65,7 +66,10 @@ export class TemplateStepThreeConfigComponent extends TemplateSetupStep {
           this.newAppContextPath = app.contextPath;
           this.newAppIcon = app.applicationBuilder.icon;
       })
-  );
+    );
+    this.app.subscribe (app => {
+      this.currentApp = app;
+    } );
   }
 
   ngOnInit() {
@@ -130,10 +134,10 @@ export class TemplateStepThreeConfigComponent extends TemplateSetupStep {
   async configureApp() {
     this.appList = (await this.appService.list({ pageSize: 2000 })).data;
     const currentHost = window.location.host.split(':')[0];
-    if (currentHost === 'localhost' || currentHost === '127.0.0.1') {
+   /*  if (currentHost === 'localhost' || currentHost === '127.0.0.1') {
         this.alert.warning("Installation isn't supported when running Application on localhost.");
         return;
-    }
+    } */
     // create Dashboard and install dependencies
     // Also connect with the devices selected
     let totalRemotes = this.configStepData.plugins.length;
@@ -144,24 +148,28 @@ export class TemplateStepThreeConfigComponent extends TemplateSetupStep {
     let overallProgress = 0;
     this.showProgressModalDialog("Verifying plugins...")
     if (totalRemotes > 1) { this.progressIndicatorService.setOverallProgress(overallProgress) }
-
+    this.progressIndicatorService.setOverallProgress(5);
     for (let plugin of this.configStepData.plugins) {
       await this.installPlugin(plugin);
       overallProgress = overallProgress + eachRemoteProgress;
       this.progressIndicatorService.setOverallProgress(overallProgress)
     };
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, 1000));
     for (let ms of this.configStepData.microservices) {
-      await this.installMicroservice(ms);
+      this.progressIndicatorService.setMessage(`Installing ${ms.title}`);
+      this.progressIndicatorService.setProgress(10);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const isInstalled = (await this.applicationBinaryService.verifyExistingMicroservices(ms.id)) as any;
+      if(!isInstalled) { await this.installMicroservice(ms); }
       overallProgress = overallProgress + eachRemoteProgress;
       this.progressIndicatorService.setOverallProgress(overallProgress)
     };
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, 1000));
     for (let db of this.configStepData.dashboards) {
       this.progressIndicatorService.setProgress(20);
-      this.progressIndicatorService.setMessage(`Installing ${db.name}`);
-      
-      const templateDetails = await this.loadTemplateDetails(db);
+      this.progressIndicatorService.setMessage(`Installing ${db.title}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const templateDetails = await (await this.loadTemplateDetails(db)).toPromise();
       const dashboardConfiguration = {
         dashboardId: '12598412',
         dashboardName: db.title,
@@ -172,7 +180,7 @@ export class TemplateStepThreeConfigComponent extends TemplateSetupStep {
         roles: ''
       };
       this.progressIndicatorService.setProgress(40);
-      await this.catalogService.createDashboard(this.app, dashboardConfiguration, db, templateDetails);
+      await this.catalogService.createDashboard(this.currentApp, dashboardConfiguration, db, templateDetails);
       this.progressIndicatorService.setProgress(90);
       overallProgress = overallProgress + eachRemoteProgress;
       this.progressIndicatorService.setOverallProgress(overallProgress)
@@ -186,94 +194,90 @@ export class TemplateStepThreeConfigComponent extends TemplateSetupStep {
   }
 
   async installMicroservice(microService: MicroserviceDetails): Promise<void> {
-    this.progressIndicatorService.setMessage(`Installing ${microService.title}`);
-    this.progressIndicatorService.setProgress(10);
-    await new Promise(resolve => setTimeout(resolve, 1000));
     let counter = 10;
     this.microserviceDownloadProgress$ = this.microserviceDownloadProgress.subscribe(async val => {
-        counter++;
-        if (counter <= 40) {
-            this.progressIndicatorService.setProgress(counter);
-        }
+      counter++;
+      if (counter <= 40) {
+        this.progressIndicatorService.setProgress(counter);
+      }
     });
-    this.templateCatalogSetupService.downloadBinary(microService.link)
-        .subscribe(async data => {
-            let createdApp = null;
-            this.microserviceDownloadProgress$.unsubscribe();
-            try {
-                this.progressIndicatorService.setProgress(40);
-                this.progressIndicatorService.setMessage(`Installing ${microService.title}`);
-                const blob = new Blob([data], {
-                    type: 'application/zip'
-                });
-                const fileName = microService.link.replace(/^.*[\\\/]/, '');
-                const fileOfBlob = new File([blob], fileName);
+    const data = await this.templateCatalogSetupService.downloadBinary(microService.link).toPromise();
+    let createdApp = null;
+    this.microserviceDownloadProgress$.unsubscribe();
+    try {
+      this.progressIndicatorService.setProgress(40);
+      this.progressIndicatorService.setMessage(`Installing ${microService.title}`);
+      const blob = new Blob([data], {
+        type: 'application/zip'
+      });
+      const fileName = microService.link.replace(/^.*[\\\/]/, '');
+      const fileOfBlob = new File([blob], fileName);
 
-                const createdApp = await this.applicationBinaryService.createAppForArchive(fileOfBlob);
-                this.progressIndicatorService.setProgress(50);
-                counter = 50;
-                this.microserviceDownloadProgress$ = this.microserviceDownloadProgress.subscribe(async val => {
-                    counter++;
-                    if (counter <= 80) {
-                        this.progressIndicatorService.setProgress(counter);
-                    }
-                });
-                await this.applicationBinaryService.uploadMicroservice(fileOfBlob, createdApp);
-                this.microserviceDownloadProgress$.unsubscribe();
-                this.progressIndicatorService.setProgress(80);
-                await new Promise(resolve => setTimeout(resolve, 2000));
-            } catch (ex) {
-                this.applicationBinaryService.cancelAppCreation(createdApp);
-                createdApp = null;
-                this.alert.danger("There is some technical error! Please try after sometime.");
-                console.error(ex.message);
-                /* // prepare translation of static message if it exists
-                const staticErrorMessage =
-                    ERROR_MESSAGES[ex.message] && this.translateService.instant(ERROR_MESSAGES[ex.message]);
-                // if there is no static message, use dynamic one from the exception
-                this.errorMessage = staticErrorMessage ?? ex.message;
-                if (!this.errorMessage && !this.uploadCanceled) {
-                    this.alertService.addServerFailure(ex);
-                } */
-            }
-        });
+      const createdApp = await this.applicationBinaryService.createAppForArchive(fileOfBlob);
+      this.progressIndicatorService.setProgress(50);
+      counter = 50;
+      this.microserviceDownloadProgress$ = this.microserviceDownloadProgress.subscribe(async val => {
+        counter++;
+        if (counter <= 80) {
+          this.progressIndicatorService.setProgress(counter);
+        }
+      });
+      await this.applicationBinaryService.uploadMicroservice(fileOfBlob, createdApp);
+      this.microserviceDownloadProgress$.unsubscribe();
+      this.progressIndicatorService.setProgress(80);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    } catch (ex) {
+      this.applicationBinaryService.cancelAppCreation(createdApp);
+      createdApp = null;
+      this.alert.danger("There is some technical error! Please try after sometime.");
+      console.error(ex.message);
+      /* // prepare translation of static message if it exists
+      const staticErrorMessage =
+          ERROR_MESSAGES[ex.message] && this.translateService.instant(ERROR_MESSAGES[ex.message]);
+      // if there is no static message, use dynamic one from the exception
+      this.errorMessage = staticErrorMessage ?? ex.message;
+      if (!this.errorMessage && !this.uploadCanceled) {
+          this.alertService.addServerFailure(ex);
+      } */
+    }
   }
+
   async installPlugin(plugin: PluginDetails): Promise<void> {
 
-        const widgetBinaryFound = this.appList.find(app => app.manifest?.isPackage && (app.name.toLowerCase() === plugin.title?.toLowerCase() ||
-            (app.contextPath && app.contextPath?.toLowerCase() === plugin?.contextPath?.toLowerCase())));
-        this.progressIndicatorService.setMessage(`Installing ${plugin.title}`);
-        this.progressIndicatorService.setProgress(10);
-        if (widgetBinaryFound) {
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            this.progressIndicatorService.setProgress(30);
-            this.widgetCatalogService.updateRemotesInCumulocityJson(widgetBinaryFound).then(async () => {
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }, error => {
-                this.alert.danger("There is some technical error! Please try after sometime.");
-                console.error(error);
-            });
-        } else {
-            this.progressIndicatorService.setProgress(10);
-            this.templateCatalogSetupService.downloadBinary(plugin.link)
-                .subscribe(data => {
-                    this.progressIndicatorService.setProgress(20);
-                    const blob = new Blob([data], {
-                        type: 'application/zip'
-                    });
-                    const fileName = plugin.link.replace(/^.*[\\\/]/, '');
-                    const fileOfBlob = new File([blob], fileName);
-                    this.widgetCatalogService.installPackage(fileOfBlob).then(async () => {
-                        await new Promise(resolve => setTimeout(resolve, 5000));
-                    }, error => {
-                        this.alert.danger("There is some technical error! Please try after sometime.");
-                        console.error(error);
-                    });
-                });
-        }
-        
+    const widgetBinaryFound = this.appList.find(app => app.manifest?.isPackage && (app.name.toLowerCase() === plugin.title?.toLowerCase() ||
+      (app.contextPath && app.contextPath?.toLowerCase() === plugin?.contextPath?.toLowerCase())));
+    this.progressIndicatorService.setMessage(`Installing ${plugin.title}`);
+    this.progressIndicatorService.setProgress(10);
+    if (widgetBinaryFound) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      this.progressIndicatorService.setProgress(30);
+      await this.widgetCatalogService.updateRemotesInCumulocityJson(widgetBinaryFound).then(async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      }, error => {
+        this.alert.danger("There is some technical error! Please try after sometime.");
+        console.error(error);
+      });
+    } else {
+      this.progressIndicatorService.setProgress(10);
+      const data = await this.templateCatalogSetupService.downloadBinary(plugin.link).toPromise();
+
+      this.progressIndicatorService.setProgress(20);
+      const blob = new Blob([data], {
+        type: 'application/zip'
+      });
+      const fileName = plugin.link.replace(/^.*[\\\/]/, '');
+      const fileOfBlob = new File([blob], fileName);
+      this.widgetCatalogService.installPackage(fileOfBlob).then(async () => {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      }, error => {
+        this.alert.danger("There is some technical error! Please try after sometime.");
+        console.error(error);
+      });
+
+    }
+
   }
-  async loadTemplateDetails(db: Dashboards): Promise<any> {
+  async loadTemplateDetails(db: Dashboards): Promise<Observable<any>> {
     return this.catalogService.getTemplateDetails(db.dashboard)
         .pipe(catchError(err => {
             console.log('Dashboard Catalog Details: Error in primary endpoint! using fallback...');
